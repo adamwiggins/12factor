beec67493782615c1d6636c95622cd8768999546
Max Beizer <mbeizer@heroku.com>
2016-08-17 10:19:03 -0500

    Force SSL with rack-ssl-enforcer (#113)
    * Add rack-ssl-enforcer
    
    * Use config var to conditionally force SSL

--- web.rb
+++ web.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 require 'maruku'
 require 'i18n'
+require 'rack/ssl-enforcer'
 
 configure do
+  use Rack::SslEnforcer if ENV['FORCE_SSL']
   I18n.enforce_available_locales = true
   I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
   I18n.backend.load_translations
